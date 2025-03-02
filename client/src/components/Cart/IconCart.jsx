3c0eeaad1debfd21d97ed3b585c17f80968f2233
evgheni.cernev <evgheni.cernev@klarna.com>
2025-03-02 23:20:34 +0200

    added styles

--- client/src/components/Cart/IconCart.jsx
+++ client/src/components/Cart/IconCart.jsx
@@ -3,7 +3,7 @@ import i18next from "i18next";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Popover from "../../elements/Popover";
 import EmptyCartModal from "./EmptyCartModal";
 import ModalCart from "./ModalCart";
@@ -12,6 +12,12 @@ const IconCart = () => {
   const { t } = useTranslation();
   const cart = useSelector((state) => state.cart);
 
+  const { pathname } = useLocation();
+
+  if (pathname.includes("cart")) {
+    return null;
+  }
+
   return (
     <Popover
       contentWidth={300}
@@ -19,15 +25,15 @@ const IconCart = () => {
       chevronPosition={{ left: 150 }}
       content={<div>{cart.length ? <ModalCart /> : <EmptyCartModal />}</div>}
     >
-        <Link to={`/${i18next.language}/cart`}>
-          <ShoppingCartIcon className="h-8 w-8 text-white ml-10" />
+      <Link to={`/${i18next.language}/cart`}>
+        <ShoppingCartIcon className="h-8 w-8 text-white ml-10" />
 
-          {cart.length >= 0 && (
-            <div className="absolute -top-2 -right-1.5 rounded-full w-6 h-6 bg-red-700 text-white flex items-center justify-center">
-              {cart.length}
-            </div>
-          )}
-        </Link>
+        {cart.length >= 0 && (
+          <div className="absolute -top-2 -right-1.5 rounded-full w-6 h-6 bg-red-700 text-white flex items-center justify-center">
+            {cart.length}
+          </div>
+        )}
+      </Link>
     </Popover>
   );
 };
